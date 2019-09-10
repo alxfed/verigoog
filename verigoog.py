eb480e0f5e540949b734fe82bc214c99a8a3c666
Alex Fedotov <alxfed@gmail.com>
2019-09-10 17:39:41 -0500

    first commit

---
+++ verigoog.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Verification of companies with names and at least a part
+of address with the help of elgoog Place Search API
+"""
+import sqlite3
+import googlemaps
+
+
+
+def main():
+    """
+    :return: 
+    """
+    pass
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
